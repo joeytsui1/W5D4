34a494d1a8b9460e883df5e27441ae2dbf09f1b5
Mlkz <maho.mamulashvili@gmail.com>
2022-12-08 15:18:16 -0500

    Add Course

--- practice-for-ch-sql-associations-long-practice-main/app/models/course.rb
+++ practice-for-ch-sql-associations-long-practice-main/app/models/course.rb
@@ -10,4 +10,14 @@
 #  updated_at    :datetime         not null
 #
 class Course < ApplicationRecord
+    has_many :enrollments,
+        primary_key: :id,
+        foreign_key: :course_id,
+        class_name: :Enrollment
+
+    has_many :enrolled_students,
+      through: :enrollments,
+      source: :student
+
+    h
 end
