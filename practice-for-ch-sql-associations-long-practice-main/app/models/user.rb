68f3372bbc89ff84b7ba54f3ff8ff35cf15e600a
Joey Tsui <joeytsui131@gmail.com>
2022-12-08 15:00:45 -0500

    project

--- practice-for-ch-sql-associations-long-practice-main/app/models/user.rb
+++ practice-for-ch-sql-associations-long-practice-main/app/models/user.rb
@@ -8,4 +8,12 @@
 #  updated_at :datetime         not null
 #
 class User < ApplicationRecord
+    has_many :enrolled_courses,
+        primary_key: :id,
+        foreign_key: :course_id,
+        class_name: :Enrollment
+
+    has_many :courses,
+        through: :enrolled_courses,
+        source: :course
 end
